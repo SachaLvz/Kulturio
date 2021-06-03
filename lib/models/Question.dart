08dfa544d071620c9c5ba438788a96b8eb53cb63
SachaLvz <levenez.sacha@gmail.com>
2021-06-03 15:12:58 +0200

    Add Question System

--- lib/models/Question.dart
+++ lib/models/Question.dart
@@ -6,4 +6,29 @@ class Question {
   Question({this.id, this.question, this.answer, this.options});
 }
 
-const List sample_data = [];
+const List sample_data = [
+  {
+    "id": 1,
+    "question": "Qui a 13 LDC ?",
+    "options": ['Real Madrid', 'OM', 'Barcelone', 'PSG'],
+    "answer_index": 0,
+  },
+  {
+    "id": 2,
+    "question": "When google release Flutter.",
+    "options": ['Jun 2017', 'Jun 2017', 'May 2017', 'May 2018'],
+    "answer_index": 2,
+  },
+  {
+    "id": 3,
+    "question": "A memory location that holds a single letter or number.",
+    "options": ['Double', 'Int', 'Char', 'Word'],
+    "answer_index": 2,
+  },
+  {
+    "id": 4,
+    "question": "What command do you use to output data to the screen?",
+    "options": ['Cin', 'Count>>', 'Cout', 'Output>>'],
+    "answer_index": 2,
+  },
+];
